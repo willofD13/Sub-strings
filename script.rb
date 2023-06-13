3ac915eeb9f4616679743fae141cd3716354a7bb
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-06-13 15:15:31 +0300

    Erase nil values from new_arr

--- script.rb
+++ script.rb
@@ -4,6 +4,7 @@ def substrings (string, dictionary)
                  substr
               end
             end
+  new_arr.compact
 end 
 
 substrings("Howdy partner, sit down! How's it going?", ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"])
