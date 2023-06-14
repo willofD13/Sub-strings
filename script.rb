ceb55b5c48e301cafc91457f53c070f05d463585
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-06-14 13:39:08 +0300

    Take an array with substrings repeated accordingly with times array

--- script.rb
+++ script.rb
@@ -1,6 +1,8 @@
 def substrings (string, array)
-  new_arr = array.map do |substr|
+  times = array.map do |substr|
              string.scan(/(?=#{substr})/).count
-            end 
+            end
+  counted = array.zip(times).flat_map { |string,count| Array.new(count) {string}}
+end
 
 substrings("Howdy partner, sit down! How's it going?", ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"])
