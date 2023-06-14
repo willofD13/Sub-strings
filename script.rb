206697691fc9f117d34ae0987dffaee766e8c975
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-06-14 13:43:26 +0300

    Take the final hash with substrings numbered

--- script.rb
+++ script.rb
@@ -3,6 +3,10 @@ def substrings (string, array)
              string.scan(/(?=#{substr})/).count
             end
   counted = array.zip(times).flat_map { |string,count| Array.new(count) {string}}
+  counted.reduce(Hash.new(0)) do |result, substr|
+    result[substr] += 1
+    p result
+  end
 end
 
 substrings("Howdy partner, sit down! How's it going?", ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"])
