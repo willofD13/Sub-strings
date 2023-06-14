98cdf015fe7875e2f0e0586eae71bfbf83ddb8c5
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-06-14 13:31:37 +0300

    Take a new array with  the times each substring was found

--- script.rb
+++ script.rb
@@ -1,10 +1,6 @@
 def substrings (string, array)
   new_arr = array.map do |substr|
-              if string.downcase.include?(substr)
-                 substr
-              end
-            end
-  p new_arr.compact
-end 
+             string.scan(/(?=#{substr})/).count
+            end 
 
 substrings("Howdy partner, sit down! How's it going?", ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"])
