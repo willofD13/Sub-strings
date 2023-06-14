179ae8c304ebf5a7bd9905bf21ddbcae4ad01e84
Leonidas Tsiakas <leonidastsiakas13@gmail.com>
2023-06-14 13:18:25 +0300

    Change second argument

--- script.rb
+++ script.rb
@@ -1,10 +1,10 @@
-def substrings (string, dictionary)
-  new_arr = dictionary.map do |substr|
+def substrings (string, array)
+  new_arr = array.map do |substr|
               if string.downcase.include?(substr)
                  substr
               end
             end
-  new_arr.compact
+  p new_arr.compact
 end 
 
 substrings("Howdy partner, sit down! How's it going?", ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"])
